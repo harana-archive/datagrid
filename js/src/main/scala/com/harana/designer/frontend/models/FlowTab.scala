a84ba60826ed4bf1e507d8da4e0784df7bb35e32
Naden <naden@harana.com>
2024-03-02 07:02:33 +1100

    Updates

--- js/src/main/scala/com/harana/designer/frontend/models/FlowTab.scala
+++
@@ -1,9 +0,0 @@
-package com.harana.designer.frontend.models
-
-sealed trait FlowTab
-object FlowTab {
-  case object Actions extends FlowTab
-  case object Parameters extends FlowTab
-  case object Execution extends FlowTab
-  case object Logs extends FlowTab
-}
